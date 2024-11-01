3714232429a962d604653c91bf65dc3259c35bb2
Samir Alholo <samir.alholo@wizme.com>
2024-11-01 11:00:41 +0300

    Setting up app route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRoutes } from './configs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: AppRoutes.Dashboard,
+    pathMatch: 'full',
+  },
+  {
+    path: AppRoutes.Dashboard,
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
