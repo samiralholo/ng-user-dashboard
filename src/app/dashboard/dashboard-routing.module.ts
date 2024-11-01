6d2984bc846c327cb54b860274cab11f36c10f54
Samir Alholo <samir.alholo@wizme.com>
2024-11-01 10:57:55 +0300

    Init Dashboard Module & Component

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { AppRoutes } from '@ng-user-dashboard/configs';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: AppRoutes.Users,
+        loadChildren: () =>
+          import('./users/users.module').then((m) => m.UsersModule),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class DashboardRoutingModule {}
