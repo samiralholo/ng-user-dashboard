61fb87bb33301578973950df3857fbc66bcf24e4
Samir Alholo <samir.alholo@wizme.com>
2024-11-01 11:09:10 +0300

    Add Missing User Routing Module

--- src/app/dashboard/users/users.module.ts
+++ src/app/dashboard/users/users.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
-
-
+import { UsersRoutingModule } from './users-routing.module';
 
 @NgModule({
-  declarations: [
-    UsersListComponent,
-    UserDetailsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UsersListComponent, UserDetailsComponent],
+  imports: [CommonModule, UsersRoutingModule],
 })
-export class UsersModule { }
+export class UsersModule {}
