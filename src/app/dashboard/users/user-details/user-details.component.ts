6bb522775eb2d1b17d8e8588ee0d8d50d3911412
Samir Alholo <samir.alholo@wizme.com>
2024-11-02 11:52:30 +0300

    Complete User Details Design

--- src/app/dashboard/users/user-details/user-details.component.ts
+++ src/app/dashboard/users/user-details/user-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@ng-user-dashboard/models';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrl: './user-details.component.scss'
+  styleUrl: './user-details.component.scss',
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  user: User | null = null;
 
+  ngOnInit(): void {
+    this.user = {
+      id: 1,
+      first_name: 'first_name',
+      last_name: 'last_name',
+      email: 'email',
+      avatar:
+        'https://lumiere-a.akamaihd.net/v1/images/a_avatarpandorapedia_neytiri_16x9_1098_01_0e7d844a.jpeg',
+    };
+  }
 }
