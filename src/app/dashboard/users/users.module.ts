75239b2da0fdb7c8c84ab89ef7313dcba5200116
Samir Alholo <samir.alholo@wizme.com>
2024-11-02 11:53:42 +0300

    Add Necessary Imports

--- src/app/dashboard/users/users.module.ts
+++ src/app/dashboard/users/users.module.ts
@@ -3,9 +3,25 @@ import { CommonModule } from '@angular/common';
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UsersRoutingModule } from './users-routing.module';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [UsersListComponent, UserDetailsComponent],
-  imports: [CommonModule, UsersRoutingModule],
+  imports: [
+    CommonModule,
+    UsersRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class UsersModule {}
