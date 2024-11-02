37bde37050196690324c5fe12c6304059a668aa0
Samir Alholo <samir.alholo@wizme.com>
2024-11-02 19:35:44 +0300

    Implement HTTP Cache

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,22 +1,48 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GResult, PaginationResult, User } from '@ng-user-dashboard/models';
-import { lastValueFrom, Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { shareReplay, catchError } from 'rxjs/operators';
 import { BASE_API } from '@ng-user-dashboard/configs';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   baseAPI = BASE_API;
 
+  private usersListCache: {
+    [page: number]: Observable<PaginationResult<User>>;
+  } = {};
+  private userDetailsCache: { [id: number]: Observable<GResult<User>> } = {};
+
   constructor(private httpClient: HttpClient) {}
 
   getUsersList(pageNumber: number): Observable<PaginationResult<User>> {
-    return this.httpClient.get<PaginationResult<User>>(
-      `${this.baseAPI}?page=${pageNumber}`
-    );
+    if (!this.usersListCache[pageNumber]) {
+      this.usersListCache[pageNumber] = this.httpClient
+        .get<PaginationResult<User>>(`${this.baseAPI}?page=${pageNumber}`)
+        .pipe(
+          shareReplay(1),
+          catchError((error) => {
+            delete this.usersListCache[pageNumber];
+            throw error;
+          })
+        );
+    }
+    return this.usersListCache[pageNumber];
   }
 
   getUserDetails(userId: number): Observable<GResult<User>> {
-    return this.httpClient.get<GResult<User>>(`${this.baseAPI}/${userId}`);
+    if (!this.userDetailsCache[userId]) {
+      this.userDetailsCache[userId] = this.httpClient
+        .get<GResult<User>>(`${this.baseAPI}/${userId}`)
+        .pipe(
+          shareReplay(1),
+          catchError((error) => {
+            delete this.userDetailsCache[userId];
+            throw error;
+          })
+        );
+    }
+    return this.userDetailsCache[userId];
   }
 }
