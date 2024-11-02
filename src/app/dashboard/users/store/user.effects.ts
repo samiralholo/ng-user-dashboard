2515d961c3a598072e43d64ce471cfc17c4b0631
Samir Alholo <samir.alholo@wizme.com>
2024-11-02 19:36:26 +0300

    Add Loading Actions

--- src/app/dashboard/users/store/user.effects.ts
+++ src/app/dashboard/users/store/user.effects.ts
@@ -1,8 +1,7 @@
 ﻿import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, concatMap } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { map, concatMap, tap, finalize } from 'rxjs/operators';
 import { UserActions } from './user.actions';
 import { UserService } from '../../../shared/services/user.service';
 
@@ -10,14 +9,17 @@ import { UserService } from '../../../shared/services/user.service';
 export class UserEffects {
   private readonly actions$ = inject(Actions);
   private readonly userService = inject(UserService);
+  private readonly store = inject(Store);
 
   getUsersList$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UserActions.getUsersList),
+      tap(() => this.store.dispatch(UserActions.setIsLoading(true))),
       concatMap((action: any) =>
-        this.userService
-          .getUsersList(action.pageNumber)
-          .pipe(map((usersList) => UserActions.getUsersListSuccess(usersList)))
+        this.userService.getUsersList(action.pageNumber).pipe(
+          map((usersList) => UserActions.getUsersListSuccess(usersList)),
+          finalize(() => this.store.dispatch(UserActions.setIsLoading(false)))
+        )
       )
     )
   );
@@ -25,12 +27,12 @@ export class UserEffects {
   getUserDetails$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UserActions.getUserDetails),
+      tap(() => this.store.dispatch(UserActions.setIsLoading(true))),
       concatMap((action: any) =>
-        this.userService
-          .getUserDetails(action.userId)
-          .pipe(
-            map((userDetails) => UserActions.getUserDetailsSuccess(userDetails))
-          )
+        this.userService.getUserDetails(action.userId).pipe(
+          map((userDetails) => UserActions.getUserDetailsSuccess(userDetails)),
+          finalize(() => this.store.dispatch(UserActions.setIsLoading(false)))
+        )
       )
     )
   );
